2b94fef12d0e78a71813bb5c9e79b3a9ebe2b212
Chris Joakim <christopher.joakim@gmail.com>
2021-08-31 08:49:45 -0400

    az/delete_rg.ps1

---
+++ az/delete_rg.ps1
@@ -0,0 +1,12 @@
+# Delete the Azure Resource Group associated with this project.
+# Chris Joakim, Microsoft, August 2021
+
+echo "deleting resource group: "$Env:cosmos_sql_rg
+
+az group delete `
+    --name $Env:cosmos_sql_rg `
+    --subscription $Env:AZURE_SUBSCRIPTION_ID `
+    --yes `
+    > tmp/delete_rg.json
+
+echo 'done'
