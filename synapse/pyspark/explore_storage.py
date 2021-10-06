4fbd784a5b502afb920892ce011ef9cd6ee995bb
Chris Joakim <christopher.joakim@gmail.com>
2021-10-06 14:25:36 -0400

    synapse/pyspark/explore_storage

--- synapse/pyspark/explore_storage.py
+++ synapse/pyspark/explore_storage.py
@@ -59,7 +59,7 @@ wasb_path = 'wasbs://%s@%s.blob.core.windows.net/%s' % (blob_container_name, blo
 print('wasb_path: {}'.format(wasb_path))
 
 df.coalesce(1).write.csv(wasb_path, mode='overwrite', header='true')
-print('df written to blob storage')
+print('df written to blob storage, with coalesce')
 
 df2 = spark.read.option("header", "true")             .option("delimiter",",")             .csv(wasb_path)
 
@@ -71,9 +71,21 @@ display(df2.limit(100))
 
 
 
+# Azure Storage account and blob info:
+blob_account_name = 'cjoakimstorage' # replace with your blob name
+blob_container_name = 'synapse' # replace with your container name
+blob_relative_path = 'usa-states-text.csv' # replace with your relative folder path
+linked_service_name = 'cjoakimstorageAzureBlobStorage'
 
-# In[ ]:
+blob_sas_token = mssparkutils.credentials.getConnectionStringOrCreds(linked_service_name)
+print('blob_sas_token: {}'.format(blob_sas_token))
 
+wasb_path = 'wasbs://%s@%s.blob.core.windows.net/%s' % (blob_container_name, blob_account_name, blob_relative_path)
+print('wasb_path: {}'.format(wasb_path))
 
+df2.rdd.saveAsTextFile(wasb_path)
+print('df written to blob storage, as text')
 
+# Row(id='50', code='WV', name='West Virginia')
+# Row(id='51', code='WY', name='Wyoming')
 
