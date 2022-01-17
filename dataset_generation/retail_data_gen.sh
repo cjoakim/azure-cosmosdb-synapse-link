03b4ce2fe0003cca8e12c223ec7c587e82a8f376
Chris Joakim <christopher.joakim@gmail.com>
2022-01-17 08:09:42 -0500

    retail_data_gen.sh

--- dataset_generation/retail_data_gen.sh
+++ dataset_generation/retail_data_gen.sh
@@ -6,6 +6,7 @@
 # Chris Joakim, Microsoft, January 2022
 
 mkdir -p data/products
+rm data/products/*.*
 
 source venv/bin/activate
 python --version
@@ -13,7 +14,22 @@ python --version
 python retail_data_gen.py create_product_catalog 12 20 90 
 python retail_data_gen.py create_stores 100
 python retail_data_gen.py create_customers 10000
-python retail_data_gen.py create_sales_data 2021-01-01 2022-01-26 100 4
+python retail_data_gen.py create_sales_data 2021-01-01 2022-01-26 75 3
+
+echo 'creating dataset.zip ...'
+zip data/products/dataset.zip data/products/*.*
+
+echo 'copying to ../DotnetConsoleApp/data/ data directory...'
+mkdir -p ../DotnetConsoleApp/data/
+cp data/products/*.csv  ../DotnetConsoleApp/data/ 
+cp data/products/*.json ../DotnetConsoleApp/data/ 
+ls -al ../DotnetConsoleApp/data/
+
+echo 'copying to ../PythonConsoleApp/data/ data directory...'
+mkdir -p ../PythonConsoleApp/data/
+cp data/products/*.csv  ../PythonConsoleApp/data/ 
+cp data/products/*.json ../PythonConsoleApp/data/ 
+ls -al ../PythonConsoleApp/data/
 
 echo 'product_catalog:'
 head -3 data/products/product_catalog.csv
@@ -30,17 +46,5 @@ wc -l   data/products/customers.csv
 echo 'sales:'
 head -3 data/products/sales.json
 wc -l   data/products/sales.json
-
-echo 'copying to ../DotnetConsoleApp/data/ data directory...'
-mkdir -p ../DotnetConsoleApp/data/
-cp data/products/*.csv  ../DotnetConsoleApp/data/ 
-cp data/products/*.json ../DotnetConsoleApp/data/ 
-ls -al ../DotnetConsoleApp/data/
-
-echo 'copying to ../PythonConsoleApp/data/ data directory...'
-mkdir -p ../PythonConsoleApp/data/
-cp data/products/*.csv  ../PythonConsoleApp/data/ 
-cp data/products/*.json ../PythonConsoleApp/data/ 
-ls -al ../PythonConsoleApp/data/
 
 echo 'done'
