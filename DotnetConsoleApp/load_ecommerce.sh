24dd06e074d468ae8332be5a2a82f87d7f3a3a8d
Chris Joakim <christopher.joakim@gmail.com>
2022-01-18 08:24:16 -0500

    Dotnet and Python load_ecommerce.sh scripts enhanced

--- DotnetConsoleApp/load_ecommerce.sh
+++ DotnetConsoleApp/load_ecommerce.sh
@@ -5,19 +5,23 @@
 # Chris Joakim, Microsoft, January 2022
 
 mkdir -p tmp/
-date > tmp/ecomm_load_start_date.txt
+date > tmp/load_times.txt
 
 dotnet run bulk_load_container demo customers customer_id data/customers.json 9999
 dotnet run bulk_load_container demo products  upc         data/product_catalog.json 9999
 dotnet run bulk_load_container demo stores    store_id    data/stores.json 9999
-dotnet run bulk_load_container demo sales     sale_id     data/sales.json 9999
+dotnet run bulk_load_container demo sales     sale_id     data/sales1.json 9999
+
+date >> tmp/load_times.txt
+cat tmp/load_times.txt
+
+# execute during demonstration:
+# dotnet run bulk_load_container demo sales     sale_id     data/sales2.json 9999
 
 dotnet run count_documents demo customers
 dotnet run count_documents demo products
 dotnet run count_documents demo stores
 dotnet run count_documents demo sales
-
-date > tmp/ecomm_load_finish_date.txt
 
 echo 'done'
 
--- PythonConsoleApp/load_ecommerce.sh
+++ PythonConsoleApp/load_ecommerce.sh
@@ -5,18 +5,22 @@
 # Chris Joakim, Microsoft, January 2022
 
 mkdir -p tmp/
-date > tmp/ecomm_load_start_date.txt
+date > tmp/load_times.txt
 
 python main.py load_container demo customers customer_id data/customers.json
 python main.py load_container demo products  upc         data/product_catalog.json
 python main.py load_container demo stores    store_id    data/stores.json
-python main.py load_container demo sales     sale_id     data/sales.json
+python main.py load_container demo sales     sale_id     data/sales1.json
+
+date >> tmp/load_times.txt
+cat tmp/load_times.txt
+
+# execute during demonstration:
+# python main.py load_container demo sales     sale_id     data/sales2.json
 
 python main.py count_documents demo customers
 python main.py count_documents demo products
 python main.py count_documents demo stores
 python main.py count_documents demo sales
-
-date > tmp/ecomm_load_finish_date.txt
 
 echo 'done'
