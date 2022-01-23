40a8b770c5b3bdc419c24949dc30acf9cfac5f23
Chris Joakim <christopher.joakim@gmail.com>
2022-01-23 13:41:38 -0500

    recreate_demo_db.sh with 1/23 document counts and elapsed

--- DotnetConsoleApp/recreate_demo_db.sh
+++ DotnetConsoleApp/recreate_demo_db.sh
@@ -37,36 +37,10 @@ dotnet run list_containers $dbname
 echo 'done'
 
 
-# Output:
-
-# $ ./recreate_demo_db.sh
-# ===
-# deleting database ...
-# GetDatabase demo -> Exception Microsoft.Azure.Cosmos.CosmosException : Response status code does not indicate success: NotFound (404); Substatus: 0; ActivityId: ce4f3dc2-f5e2-4aa1-ba7e-e726b101b964; Reason: (
-# Errors : [
-#   "Resource Not Found. Learn more: https://aka.ms/cosmosdb-tsg-not-found"
-# ... exception messages ...
-# DeleteDatabase demo -> statusCode -1
-# sleeping 20 ...
-# ===
-# creating database ...
-# CreateDatabase demo 10000 -> status code Created, RU 3
-# sleeping 20 ...
-# ===
-# creating containers ...
-# CreateContainer demo customers 0 -> Id customers
-# CreateContainer demo products 0 -> Id products
-# CreateContainer demo stores 0 -> Id stores
-# CreateContainer demo sales 0 -> Id sales
-# sleeping 20 ...
-# listing databases ...
-# demo
-# ListDatabases - count 1
-# database 1: demo current RU 1000
-# listing containers ...
-# ListContainers - count 4
-# container in db: demo -> products
-# container in db: demo -> customers
-# container in db: demo -> stores
-# container in db: demo -> sales
-# done
+# Partial Output:
+# Sun Jan 23 13:34:46 EST 2022
+# Sun Jan 23 13:37:01 EST 2022
+# CountDocuments demo customers -> 10000
+# CountDocuments demo products -> 20894
+# CountDocuments demo stores -> 100
+# CountDocuments demo sales -> 101159
