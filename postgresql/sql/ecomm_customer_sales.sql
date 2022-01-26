c7c5bdb6c1be12bb82bb3aeba0beb22bd7b78648
Chris Joakim <christopher.joakim@gmail.com>
2022-01-26 05:27:15 -0500

    cosmos_mongo_sales_processing.ipynb

--- postgresql/sql/ecomm_customer_sales.sql
+++ postgresql/sql/ecomm_customer_sales.sql
@@ -1,15 +1,29 @@
 -- DDL to create the customer_sales table in the Azure PostgreSQL demo database.
--- Chris Joakim, Microsoft, October 2021
-
--- customer_id,id,order_count,total_dollar_amount,total_item_count
--- 0099999951721,402e3211-1ec8-45dd-818d-7c563574e1de,4,527.05,9
+-- Chris Joakim, Microsoft, January 2022
 
 drop table customer_sales;
 
 CREATE TABLE "customer_sales" (
-	"customer_id"         character varying(80) not null,
-	"id"                  character varying(80) not null,
+	"customer_id"         integer not null,
+	"_id"                 character varying(80) not null,
+	"pk"                  integer not null,
 	"order_count"         integer not null,
 	"total_dollar_amount" money,
 	"total_item_count"    integer not null
 );
+
+-- PySpark DataFrame Schema in Azure Synapse:
+-- root
+--  |-- customer_id: integer (nullable = true)
+--  |-- _id: integer (nullable = true)
+--  |-- pk: integer (nullable = true)
+--  |-- order_count: long (nullable = false)
+--  |-- total_dollar_amount: double (nullable = true)
+--  |-- total_item_count: long (nullable = true)
+
+-- The data looks like this:
+-- $ head part-00000-212e8938-3e37-4ae0-9075-8320e5e83adb-c000.csv
+-- customer_id,_id,pk,order_count,total_dollar_amount,total_item_count
+-- 1,1,1,1,4633.63,2
+-- 2,2,2,5,9969.06,11
+-- 3,3,3,6,27992.590000000004,17
