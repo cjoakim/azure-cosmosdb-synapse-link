76e6fe0c2db250a2a69ecaa71b33b62b3b0b2d19
Chris Joakim <christopher.joakim@gmail.com>
2022-02-05 13:38:06 -0500

    dataset generation for february demos

--- dataset_generation/retail_data_gen.sh
+++ dataset_generation/retail_data_gen.sh
@@ -3,7 +3,7 @@
 # Generate simulated and correlated "ecommerce retail" datasets 
 # consisting of products, stores, customers, orders, and line items.
 # These are for loading into CosmosDB with the DotnetConsoleApp.
-# Chris Joakim, Microsoft, January 2022
+# Chris Joakim, Microsoft, February 2022
 
 mkdir -p data/retail
 rm data/retail/*.*
@@ -14,9 +14,9 @@ python --version
 python retail_data_gen.py create_product_catalog 12 20 90 
 python retail_data_gen.py create_stores 100
 python retail_data_gen.py create_customers 10000
-python retail_data_gen.py create_sales_data 2021-01-01 2022-01-26 75 3
+python retail_data_gen.py create_sales_data 2021-01-01 2022-02-17 75 3
 
-python retail_data_gen.py slice_sales_data 2022-01-26
+python retail_data_gen.py slice_sales_data 2022-02-17
 
 echo 'removing sales.json, now split into two files ...'
 rm data/retail/sales.json
