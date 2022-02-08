04b04a70350ce3f397e787171976c6d2e6f89809
Chris Joakim <christopher.joakim@gmail.com>
2022-02-08 11:08:20 -0500

    deleted presentation/ dir, will recreate

--- postgresql/sql/packages.sql
+++
@@ -1,8 +0,0 @@
--- DDL to create the package_and_version table in the Azure PostgreSQL demo database.
--- Chris Joakim, Microsoft, February 2021
-
-drop table packages;
-
-CREATE TABLE "packages" (
-	"package_and_version" character varying(100) not null
-);
