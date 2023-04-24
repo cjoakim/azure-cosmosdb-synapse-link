ed7caf976dda173ecb9f40fca44b859fe9f0573f
Chris Joakim <christopher.joakim@gmail.com>
2023-04-24 08:41:08 -0400

    mongo_sales.ipynb writing to pg

--- Relational/PostgreSQL/sql/ecomm_customer_sales.sql
+++ Relational/PostgreSQL/sql/ecomm_customer_sales.sql
@@ -2,12 +2,21 @@
 -- demo database.
 -- Chris Joakim, Microsoft
 
+-- The Aggregated Spark Dataframe used to load this table looks like this:
+-- customer_id: integer (nullable = true)
+-- id: integer (nullable = true)
+-- pk: integer (nullable = true)
+-- order_count: long (nullable = false)
+-- total_dollar_amount: double (nullable = true)
+-- total_item_count: long (nullable = true)
+
 drop table customer_sales;
 
 CREATE TABLE "customer_sales" (
-	"id"                  character varying(80) not null,
-	"customer_id"         character varying(80) not null,
-	"order_count"         integer not null,
+	"customer_id"         integer not null,
+	"id"                  integer,
+	"pk"                  integer,
+	"order_count"         integer,
 	"total_dollar_amount" money,
-	"total_item_count"    integer not null
+	"total_item_count"    integer
 );
